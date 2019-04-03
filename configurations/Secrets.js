4eee76f43b8edc1047c0bc88b181d4dfe95ff83a
Haythem Mnasri <haythem.mnasri@esprit.tn>
2019-04-03 16:19:07 +0100

    CHANGE DATABSAE NAME "healthchain"
    CHANGE DATABSAE NAME "healthchain"

--- configurations/Secrets.js
+++ configurations/Secrets.js
@@ -1,7 +1,7 @@
 module.exports = {
   JWT_SECRET: "your secret pls!",
   SALT_VALUE: 10,
-  CONNEXTION_STRING: "mongodb://localhost/project1",
+  CONNEXTION_STRING: "mongodb://localhost/healthchain",
   GMAIL_CONFIGURATIONS: {
     EMAIL_ADRESSE: "yourGmail@gmail.com",
     EMAIL_PASSWORD: "yourPasswotd",
