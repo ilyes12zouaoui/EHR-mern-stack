109dc3d7767784e1d9280e581c0888e43f7b0363
haythembelhajyoussef <43624117+haythembelhajyoussef@users.noreply.github.com>
2019-04-03 19:24:26 +0100

    Change connection string mongoDB to Atlas

--- configurations/Secrets.js
+++ configurations/Secrets.js
@@ -1,7 +1,7 @@
 module.exports = {
   JWT_SECRET: "your secret pls!",
   SALT_VALUE: 10,
-  CONNEXTION_STRING: "mongodb://localhost/healthchain",
+  CONNEXTION_STRING: "mongodb+srv://admin:admin@ehr-roxao.mongodb.net/ehr?retryWrites=true",
   GMAIL_CONFIGURATIONS: {
     EMAIL_ADRESSE: "yourGmail@gmail.com",
     EMAIL_PASSWORD: "yourPasswotd",
