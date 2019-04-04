a332b88f84cd4403f6dfdeeaef8b597553868ddc
Haythem Mnasri <haythem.mnasri@esprit.tn>
2019-04-04 20:30:24 +0100

    Update PatientSpace
    Update PatientSpace

--- models/UserModel.js
+++ models/UserModel.js
@@ -45,7 +45,92 @@ const userSchema = mongoose.Schema({
   image: {
     type: String,
     default: "defaultProfilePicture.jpg"
-  }
+  },
+
+    birthDate: {
+        type: Date,
+        //required: true
+    },
+
+    country: {
+        type: String,
+        //required: true
+    },
+
+    city: {
+        type: String,
+        //required: true
+    },
+
+    address: {
+        type: String,
+        //required: true
+    },
+
+    telNum: {
+        type: Number,
+        //required: true
+    },
+
+    gender: {
+        type: String,
+        //required: true
+    },
+
+    cin: {
+        type: String,
+        //required: true
+    },
+
+    blood_type: {
+        type: String,
+        //required: true
+    },
+
+    height: {
+        type: Number,
+        //required: true
+    },
+
+    weight: {
+        type: Number,
+        //required: true
+    },
+
+    physical_activity: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'physical_activity'
+    },
+
+    nutrition: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'nutrition'
+    },
+
+    isLoggedIn: {
+        type: Boolean,
+        //required: true
+    },
+
+    doctors: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'doctor'
+    }],
+
+    doctorsAllowed: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'doctor'
+    }],
+
+    pharmacistsAllowed: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'pharmacist'
+    }],
+
+    thirdPartiesAllowed: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'thirdPartie'
+    }],
 });
 
 userSchema.methods.generateAuthToken = function() {
