79e97db24f118f57de2e6d3dc1d011d21920ce42
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-04-04 02:29:14 +0200

    updating config

--- configurations/Secrets.js
+++ configurations/Secrets.js
@@ -1,10 +1,11 @@
 module.exports = {
   JWT_SECRET: "your secret pls!",
   SALT_VALUE: 10,
-  CONNEXTION_STRING: "mongodb://localhost/project1",
+  ONNEXTION_STRING:
+    "mongodb+srv://admin:admin@ehr-roxao.mongodb.net/ehr?retryWrites=true",
   GMAIL_CONFIGURATIONS: {
-    EMAIL_ADRESSE: "ilyes.zouaoui11@gmail.com",
-    EMAIL_PASSWORD: "vol130VOL130",
+    EMAIL_ADRESSE: "esprit.ehr@gmail.com",
+    EMAIL_PASSWORD: "Azerty0+",
     EMAIL_SERVICE: "gmail"
   },
   EMAIL_TEMPLETES: {
